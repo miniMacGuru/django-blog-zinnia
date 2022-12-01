595d34238362f3ec09686ad030328ba3d52038c1
Tiago Santos <tiagomanuelfs@hotmail.com>
2022-12-01 14:00:08 +0000

    Convert from deprecate url path logic to Django 3 path

--- zinnia/tests/implementations/urls/default.py
+++ zinnia/tests/implementations/urls/default.py
@@ -2,6 +2,7 @@
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
 
 from django_xmlrpc.views import handle_xmlrpc
 
@@ -10,9 +11,9 @@ from zinnia.views.channels import EntryChannel
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^', include('zinnia.urls')),
-    url(r'^channel-test/$', EntryChannel.as_view(query='test')),
-    url(r'^comments/', include('django_comments.urls')),
-    url(r'^xmlrpc/$', handle_xmlrpc),
-    url(r'^admin/', admin.site.urls),
+    path('', include('zinnia.urls')),
+    path('channel-test/', EntryChannel.as_view(query='test')),
+    path('comments/', include('django_comments.urls')),
+    path('xmlrpc/', handle_xmlrpc),
+    path('admin/', admin.site.urls),
 ]
