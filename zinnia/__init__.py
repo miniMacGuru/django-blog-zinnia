b6827901e67c6894e761c23949db68ceb8b3db31
Tiago Santos <tiagomanuelfs@hotmail.com>
2022-12-02 17:53:53 +0000

    Add maintainer email

--- zinnia/__init__.py
+++ zinnia/__init__.py
@@ -3,6 +3,7 @@ __version__ = '0.22.0-hk'
 __license__ = 'BSD License'
 
 __author__ = 'Fantomas42'
+__email__ = 'tech@housekeep.com'
 
 __url__ = 'https://github.com/HousekeepLtd/django-blog-zinnia'
 
